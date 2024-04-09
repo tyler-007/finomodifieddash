6bdc6b4504a55912e1d4dfb7348eac7eee27dd41
tyler-007 <123353445+tyler-007@users.noreply.github.com>
2024-04-09 13:33:58 +0530

    Add files via upload

--- ai_interaction.py
+++ ai_interaction.py
@@ -9,7 +9,7 @@ from config import organisation as organization_name
 def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
     # Andrew mentioned that the prompt/completion paradigm is preferable for this class
     messages = [
-        {"role": "system", "content": f"You are a kind business insight employee with speciality in online media sentiment analysis, you work in {organization_name} - an organization provided by the user."},
+        {"role": "system", "content": f"You are a kind business insight employee with speciality in online media sentiment analysis, you work in  an organization provided by the user."},
         {"role": "assistant", "content": prompt}
     ]
     response = openai.ChatCompletion.create(
